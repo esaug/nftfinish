10e4f0ae1b655336db466054f07c263d125aad7a
Esau Guerra <46943313+esaug@users.noreply.github.com>
2022-04-01 13:48:01 -0400

    Add files via upload
    Minting 0.4

---
+++ precache-manifest.6aced3a72164a12e00a9275301b273f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0786a4effacf357b33c8fac9d2089e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a558293dc63ba25e4dfb",
+    "url": "/static/css/main.d928d9ff.chunk.css"
+  },
+  {
+    "revision": "6899c26117eaa74e08f2",
+    "url": "/static/js/2.62756173.chunk.js"
+  },
+  {
+    "revision": "bea1eeb7636f643b8c79f74ea8b64c89",
+    "url": "/static/js/2.62756173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a558293dc63ba25e4dfb",
+    "url": "/static/js/main.ba2beba3.chunk.js"
+  },
+  {
+    "revision": "dec26090cf87837f80e5",
+    "url": "/static/js/runtime-main.e95b8414.js"
+  }
+]);
